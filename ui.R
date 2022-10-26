5b7b36ba7d238dae2e7c92b315452e83f086acca
Drew Sorrells <sorrellsd23@mail.wlu.edu>
2022-10-26 16:17:27 -0400

    Edits

--- ui.R
+++ ui.R
@@ -1,16 +1,24 @@
 library(shiny)
 library(shinydashboard)
 
-Allflights <- readRDS(file = "all_flights.Rdata")
 
 dashboardPage(
   dashboardHeader(title = "Flight Status Predictor"),
   dashboardSidebar(
     sidebarMenuOutput("Menu")
   ),
-  dashboardBody()
+  dashboardBody(  
+    fluidRow(
+    box(plotOutput("plot1")),
+    
+    box(
+      "Flight Time Delay Status", br(), "Based on Yearly Data from 2018, 2020, 2022",
+      sliderInput("slider", "Departure Time:", 1, 2400, 1200),
+      textInput("text", "Flight Status:")
+      )
 )
-
+)
+)
 
 
 
