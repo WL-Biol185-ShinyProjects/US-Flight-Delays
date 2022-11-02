7e763e0eea299fb78dc681f962c36c940dc7f053
Dawson Kramer <kramerd23@mail.wlu.edu>
2022-11-02 15:08:38 -0400

    Content in tab 1

--- ui.R
+++ ui.R
@@ -18,27 +18,29 @@ dashboardPage(
   dashboardBody(
     tabItems(
       tabItem(tabName = "predictor",
-            h2("Flight Time Predictor")
+            h2("Flight Time Predictor"),
+            fluidRow(
+              box(plotOutput("plot1")), #plot of predicted delays 
+              
+              box(
+                "Flight Time Delay Status", br(), "Based on Yearly Data from 2018, 2020, 2022",
+                textInput("text", "Departure Time:"),
+                textInput("text", "Arrival Time:"),
+                textInput("text", "Origin (Airport):"),
+                textInput("text", "Destination (Airport):"),
+                textInput("text", "Airline:"),
+                textInput("text", "Flight Date:"),
+                textInput("text", "Flight Status:")
+              )
+            )
               ),
+      
 
       tabItem(tabName = "data",
               h2("Data")
               )
             ),
-    fluidRow(
-    box(plotOutput("plot1")), #plot of predicted delays 
     
-    box(
-      "Flight Time Delay Status", br(), "Based on Yearly Data from 2018, 2020, 2022",
-      textInput("text", "Departure Time:"),
-      textInput("text", "Arrival Time:"),
-      textInput("text", "Origin (Airport):"),
-      textInput("text", "Destination (Airport):"),
-      textInput("text", "Airline:"),
-      textInput("text", "Flight Date:"),
-      textInput("text", "Flight Status:")
-      )
-)
 )
 )
 
