36191d0775137b338fed62523d64793ed663f8fb
Tyler <bernardt23@mail.wlu.edu>
2022-11-09 15:16:09 -0500

    trying to make the ggplot work properly - changing filter to compare directly with input variables

--- server.R
+++ server.R
@@ -6,21 +6,26 @@ flights <- readRDS("all_flights_clean.Rdata")
 flights2 <- flights[-c(17:37)]
 flights2 <- flights2[-c(11:16)]
 flights2 <- flights2[-c(5:8)]
+flights2$FlightDate<-as.Date(flights2$FlightDate, format = "%m/%d/%Y")
+flights2$year_day <- yday(flights2$FlightDate)
 
 function(input, output) {
   
-  O <- (output$value <- renderText({ input$origin }))
-  D <- (output$value <- renderText({ input$destination }))
-  A <- (output$value <- renderText({ input$airline }))
+ ## O <- (output$value <- renderText({ input$origin }))
+ ## O <- as.character(O)
+ ## D <- (output$value <- renderText({ input$destination }))
+ ## D <- as.character(D)
+ ## A <- (output$value <- renderText({ input$airline }))
+ ## A <- as.character(A)
  ## dates <- yday(output$value <- renderText({ input$date }))
   
   
   output$plot1 <- renderPlot( {
     flights2 %>%
-      filter(Airline == A, Origin == O, Destination == D, year_day == dates) %>%
+      filter(Airline == input$airline, Origin == input$origin, Dest == input$destination, year_day == yday(input$date)) %>%
       group_by(year_day, Airline, Origin, Dest) %>%
       summarise(ave_delay = mean(DepDelayMinutes, na.rm = TRUE)) %>%
-      ggplot(aes(dates, ave_delay)) +
+      ggplot(aes(year_day, ave_delay)) +
       geom_point()
   }
   )
