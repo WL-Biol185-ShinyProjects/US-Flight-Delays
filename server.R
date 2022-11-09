5cd6bc514f37d2d83543b82679ec53d4afc6e556
Tyler <bernardt23@mail.wlu.edu>
2022-11-09 14:46:50 -0500

    removing columns not needed for this step of analysis so the server stops crashing

--- server.R
+++ server.R
@@ -3,8 +3,28 @@ library(leaflet)
 library(tidyverse)
 library(lubridate)
 flights <- readRDS("all_flights_clean.Rdata")
+flights2 <- flights[-c(17:37)]
+flights2 <- flights2[-c(11:16)]
+flights2 <- flights2[-c(5:8)]
 
 function(input, output) {
+  
+  O <- (output$value <- renderText({ input$origin }))
+  D <- (output$value <- renderText({ input$destination }))
+  A <- (output$value <- renderText({ input$airline }))
+ ## dates <- yday(output$value <- renderText({ input$date }))
+  
+  
+  output$plot1 <- renderPlot( {
+    flights2 %>%
+      filter(Airline == A, Origin == O, Destination == D, year_day == dates) %>%
+      group_by(year_day, Airline, Origin, Dest) %>%
+      summarise(ave_delay = mean(DepDelayMinutes, na.rm = TRUE)) %>%
+      ggplot(aes(dates, ave_delay)) +
+      geom_point()
+  }
+  )
+  
   output$menu <- renderMenu({
     sidebarMenu(
       menuItem("Menu item", icon = icon("calendar"))
@@ -16,20 +36,6 @@ function(input, output) {
     setView(lng=-95.15, lat =40 , zoom=3.5)
   m
   })
-  O <- (output$value <- renderText({ input$origin }))
-  D <- (output$value <- renderText({ input$destination }))
-  A <- (output$value <- renderText({ input$airline }))
-  dates <- yday(output$value <- renderText({ input$date }))
-<<<<<<< HEAD
-
-=======
-  flights$year_day <- yday(flights$FlightDate)  
-  flights %>%
-    group_by(year_day, Airline, Origin, Dest) %>%
-    select(c(O, D, A, dates))
-  summarise(ave_delay = mean(DepDelayMinutes, na.rm = TRUE)) %>%
-    ggplot(aes(input$date, ave_delay)) +
-    geom_point()
->>>>>>> 4fcdc1cd81d78f0d4cf92140502df4f36c2467bf
+ 
   }
 
