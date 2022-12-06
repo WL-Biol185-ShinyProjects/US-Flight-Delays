aead51e1f797d364bf9422ee5027df923c037f8d
Tyler <bernardt23@mail.wlu.edu>
2022-12-05 21:50:31 -0500

    creating 3rd graph and output plot in ui

--- server.R
+++ server.R
@@ -43,15 +43,27 @@ loaded_flights() %>%
         ggplot(aes(
           Origin,
           DepDelayMinutes)) +
-        labs(y = "Frequency of Delay",
-             x = "Departure Delay Minutes") +
+        labs(y = "Delay Minutes",
+             x = "Origin") +
       geom_tile()
       
       
 
   })
   
-    
+    output$plot3 <- renderPlot( {
+      
+      loaded_flights() %>%
+        
+        filter(loaded_flights()$Dest == input$destination2) %>%
+        ggplot(aes(
+          Dest,
+          DepDelayMinutes)) +
+        labs(y = "Departure Delay Minutes",
+             x = "Destination") +
+        geom_tile()
+      
+    })
     
   output$menu <- renderMenu({
     sidebarMenu(
