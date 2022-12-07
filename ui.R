b0b122552122149ccb39147e9853d0b83e5af12f
Tyler <bernardt23@mail.wlu.edu>
2022-12-07 15:47:12 -0500

    adding line breaks

--- ui.R
+++ ui.R
@@ -217,15 +217,19 @@ dashboardPage(
                 box(
                   "This project features data from 2018, 2020, and 2022. Users can view historical delay data from 
                   most US airports in order to make the most informed decisions possible about the destination or 
-                  departure point through which they want to fly.
+                  departure point through which they want to fly.",
                   
-                  Furthermore, users can select the destination airport, departure airport, and their desired flight 
-                  date so as to see which dates have historically been the worst (or best) to fly on.
+                  "Furthermore, users can select the destination airport, departure airport, and their desired flight 
+                  date so as to see which dates have historically been the worst (or best) to fly on.",
                   
-                  Lastly, users can view a map showing information on US airports, and both peruse and download the
-                  data involved in the project.
+                  "Lastly, users can view a map showing information on US airports, and both peruse and download the
+                  data involved in the project.",
                   
-                  Users can see the original data here: https://www.kaggle.com/datasets/robikscube/flight-delay-dataset-20182022?resource=download&select=Combined_Flights_2022.parquet"
+                  "Users can see the original data here: https://www.kaggle.com/datasets/robikscube/flight-delay-dataset-20182022?resource=download&select=Combined_Flights_2022.parquet",
+                  
+                  "They can also look at the information on airports here: https://CRAN.R-project.org/package=airportr"
+                  
+                  
                 )
               )
               )
